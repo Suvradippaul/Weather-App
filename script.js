50819e8ee7dd3aaf6fc358393787ae41744cca0e
T S Ajeet <codewithstatus200@gmail.com>
2022-10-09 17:05:45 +0530

    fixes Suvradippaul/Weather-App#3, add prompt to enter city name
    1. When an empty field is queried to search, throws a prompt to enter a city name

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const weatherField = document.getElementById('weather');
 const humidityField = document.getElementById('humidity');
 const pressureField = document.getElementById('pressure');
 const windField = document.getElementById('wind');
+const citySearchStatus = document.getElementById('city-error-message');
 
 // Clears the input field, when refreshing the page.
-cityField.value = ''
+cityField.value = '';
+cityErrorMessage.innerHTML = '';
 
 let cityName = "";
 let temp = "";
@@ -21,6 +23,7 @@ const KEY = '5313e5a818e901ec38916f96eab76e8d';
 
 cityField.addEventListener("keyup", function(event) {
     cityErrorMessage.classList.add("hide");
+    cityErrorMessage.innerHTML = '';
     if (event.key === "Enter") {
         event.preventDefault();
         search();
@@ -32,6 +35,7 @@ function search() {
     // For empty search clicks
     if(!cityName) {
       cityErrorMessage.classList.remove('hide');
+      cityErrorMessage.innerHTML = 'Enter a city name';
       hideDetails();
       return;
     }
@@ -47,6 +51,7 @@ function fetchData(cityName) {
       // Checking for cod !== 200 : As there could be other cod errors like 400, 401 etc.
       if (data.cod !== 200) {
         cityErrorMessage.classList.remove('hide');
+        cityErrorMessage.innerHTML = 'City not found';
         hideDetails();
         return;
       }
