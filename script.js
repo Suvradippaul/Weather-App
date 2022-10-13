956c65cc529b32db8bb451c5f070613c51e6b94a
Suvradippaul <suvradip2000@gmail.com>
2022-10-13 23:25:15 +0530

    feat: Sunrise/Sunset option

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const cityField = document.getElementById('city');
 const cityErrorMessage = document.getElementById('city-error-message');
 const locationField = document.getElementById('location');
+const weatherField = document.getElementById('weather');
 const tempField = document.getElementById('temp');
 const feelsLikeField = document.getElementById('feels-like');
-const weatherField = document.getElementById('weather');
+const sunriseField = document.getElementById('sunrise');
+const sunsetField = document.getElementById('sunset');
 const humidityField = document.getElementById('humidity');
 const pressureField = document.getElementById('pressure');
 const windField = document.getElementById('wind');
@@ -14,6 +16,8 @@ cityField.value = '';
 let cityName = "";
 let temp = "";
 let feels_like_temp = "";
+let sunrise = "";
+let sunset = "";
 let humidity = "";
 let pressure = "";
 let weather = "";
@@ -61,10 +65,12 @@ function fetchData(cityName) {
       temp = Math.round(temp - 273.15);
       feels_like_temp = data.main["feels_like"];
       feels_like_temp = Math.round(feels_like_temp - 273.5);
+      sunrise = convertToTime(data.sys["sunrise"]);
+      sunset = convertToTime(data.sys["sunset"]);
       humidity = data.main["humidity"];
       pressure = data.main["pressure"];
       iconId = data.weather[0]["icon"];
-      weather = data.weather[0]["main"];
+      weather = capitalise(data.weather[0]["description"]);
       // converting wind speed from m/s to km/hr
       windSpeed = (data.wind["speed"] * 3.6).toFixed(2);
       showDetails();
@@ -75,6 +81,8 @@ function showDetails() {
   locationField.innerHTML = locationName;
   tempField.innerHTML = temp + '&#176;' + 'C';
   feelsLikeField.innerHTML = 'Feels like ' + feels_like_temp + '&deg;C';
+  sunriseField.innerHTML = sunrise;
+  sunsetField.innerHTML = sunset;
   weatherField.innerHTML = weather;
   humidityField.innerHTML = humidity + '%';
   pressureField.innerHTML = pressure + ' mb';
@@ -93,3 +101,20 @@ function hideDetails() {
   pressureField.innerHTML = '___';
   windField.innerHTML = '___';
 }
+
+const convertToTime = (timestamp) => {
+  const date = new Date(timestamp * 1000);
+  const time = date.toLocaleTimeString();
+  return time.substring(0, 4) + time.substring(7,);
+}
+
+const capitalise = (sentence) => {
+  const words = sentence.split(" ");
+  const str = words.map((word) => {
+    return word[0].toUpperCase() + word.substring(1);
+  }).join(" ");
+  return str;
+}
+
+
+
