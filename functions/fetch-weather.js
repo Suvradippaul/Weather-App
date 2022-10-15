da97d636e4a8eefb71696651d2a8db3963c21732
Suvradippaul <suvradip2000@gmail.com>
2022-10-15 21:51:39 +0530

    Fix: Fetch is not defined

--- functions/fetch-weather.js
+++ functions/fetch-weather.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
+import fetch from "node-fetch"
 
 exports.handler = async (event, context) => {
     const city = event.queryStringParameters.cityName
     const API_KEY = process.env.API_KEY
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-    const response = await fetch(url)
-    const data = await response.json()
-    return {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return {
         statusCode : 200,
         body : JSON.stringify(
             {
@@ -14,4 +16,14 @@ exports.handler = async (event, context) => {
             }
         )
     }
+    }
+    catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(
+                error.toString()
+            )
+        }
+    }
+    
 }
