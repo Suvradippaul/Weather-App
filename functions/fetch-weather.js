2dbf2e9ae7e5015fe1687ed97b7c5b410ce8617a
Suvradippaul <suvradip2000@gmail.com>
2022-10-15 23:28:28 +0530

    Revert "Fix: Fetch is not defined" commit
    This reverts commit da97d636e4a8eefb71696651d2a8db3963c21732.

--- functions/fetch-weather.js
+++ functions/fetch-weather.js
@@ -1,14 +1,12 @@
 require('dotenv').config()
-import fetch from "node-fetch"
 
 exports.handler = async (event, context) => {
     const city = event.queryStringParameters.cityName
     const API_KEY = process.env.API_KEY
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        return {
+    const response = await fetch(url)
+    const data = await response.json()
+    return {
         statusCode : 200,
         body : JSON.stringify(
             {
@@ -16,14 +14,4 @@ exports.handler = async (event, context) => {
             }
         )
     }
-    }
-    catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify(
-                error.toString()
-            )
-        }
-    }
-    
 }
